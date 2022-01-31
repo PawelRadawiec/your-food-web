32e8feda376036f58bef2bc0a5c44cd25e6b1b34
PawelRadawiec <pawel.radaw@gmail.com>
2022-01-31 07:27:36 +0100

    stars list, empty stars

--- src/app/components/star-list/star-list.component.ts
+++ src/app/components/star-list/star-list.component.ts
@@ -7,32 +7,39 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class StarListComponent implements OnInit {
   @Input() rating!: number | string;
+  @Input() starsLength: number = 5;
   @Input() count!: number | string | undefined;
 
   countDisplay!: string;
+  stars: string[] = [];
   starIcons: string[] = [];
 
   constructor() {}
 
   ngOnInit() {
-    this.initStarIcons();
-    this.initCountDisplay();
+    this.stars = Array.from({ length: this.starsLength }, () => 'star');
+    this.appendStarIcons();
+    this.appendCountDisplay();
   }
 
-  initCountDisplay() {
+  appendCountDisplay() {
     if (!this.count || Number.isNaN(this.count)) return;
     this.countDisplay = `of ${this.count}`;
   }
 
-  initStarIcons() {
+  appendStarIcons() {
     const rating = Number(this.rating);
     if (Number.isNaN(rating)) return;
     const isDecimal = rating % 1 !== 0;
-    for (let i = 0; i < Math.floor(rating); i++) {
-      this.starIcons.push('star');
-    }
+    const ratingFloor = Math.floor(rating);
+    const fullStars = this.stars.slice(0, ratingFloor);
+    const emptyStars = this.stars
+      .slice(ratingFloor, this.starsLength)
+      .map((_) => 'star_outline');
+    const starIcons = fullStars.concat(emptyStars);
     if (isDecimal) {
-      this.starIcons[this.starIcons.length - 1] = 'star_half';
+      starIcons[ratingFloor] = 'star_half';
     }
+    this.starIcons = starIcons;
   }
 }
