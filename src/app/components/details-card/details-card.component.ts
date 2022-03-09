749e95571471a0f4c51a09559e944528198e4c57
PawelRadawiec <pawel.radaw@gmail.com>
2022-03-09 18:47:14 +0100

    business details - open badge

--- src/app/components/details-card/details-card.component.ts
+++ src/app/components/details-card/details-card.component.ts
@@ -1,19 +1,25 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { BusinessDetails } from 'src/app/models/business-details.model';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-details-card',
   templateUrl: './details-card.component.html',
-  styleUrls: ['./details-card.component.css']
+  styleUrls: ['./details-card.component.css'],
 })
 export class DetailsCardComponent implements OnInit {
   @Input() business: Partial<BusinessDetails> = {};
-  addresesDisplay!: string;
 
-  constructor() { }
+  addresesDisplay: string;
+  currentDayIndex: number;
+  displayOpen: boolean;
+
+  constructor() {}
 
   ngOnInit() {
     this.setAddresesDisplay();
+    this.setCurrentDayIndex();
+    this.setDisplayOpen();
   }
 
   goToPage() {
@@ -21,8 +27,15 @@ export class DetailsCardComponent implements OnInit {
   }
 
   setAddresesDisplay() {
-    const addreses = this.business.location?.display_address.join(', ');
-    this.addresesDisplay = addreses ?? '';
+    this.addresesDisplay =
+      this.business.location?.display_address.join(', ') ?? '';
   }
 
+  setDisplayOpen() {
+    this.displayOpen = this.business?.hours?.[0]?.is_open_now;
+  }
+
+  setCurrentDayIndex() {
+    this.currentDayIndex = moment().isoWeekday() - 1;
+  }
 }
